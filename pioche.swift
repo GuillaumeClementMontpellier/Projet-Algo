6cf9cca2a29fa9621bfa12d6f8b4e3965a778afd
Guillaume Clement <polig@igtp5-pc07.isim.intra>
2018-12-17 10:23:01 +0100

    Creation de la pioche2

--- pioche.swift
+++ pioche.swift
@@ -4,12 +4,37 @@ protocol pioche : piocheProtocol {
 
     typealias main = mainProtocol
 
-    
+    var liste : [carte]
 
-    init?(){
-
+    init?(j : Int){
         
+        self.liste = []
+        
+        for i in 1...9{
+            self.liste.append(carte(nom : "Soldat", joueur : j))
+        }
 
+        for i in 1...6{
+            self.liste.append(carte(nom : "Garde", joueur : j))
+        }
+
+        for i in 1...5{
+            self.liste.append(carte(nom : "Archer", joueur : j))
+        }
+
+        self.liste.shuffle()
     }
+
+    //piocher a faire (pour le royaume et pour la main)
+
+    func nbCartePioche() -> Int {
+        return self.liste.count
+    }
+
+    func estVidePioche() -> Bool {
+        return nbCartePioche == 0
+    }
+
+    
 
 }
