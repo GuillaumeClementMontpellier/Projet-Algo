f2e7119d25d289e27dc94c080a4e5cb5af6cb6e1
Etudiant IG <polig@igtp5-pc11.isim.intra>
2019-01-09 11:57:51 +0100

    PUBLIC

--- main/Package.swift
+++ main/Package.swift
@@ -14,7 +14,7 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages which this package depends on.
         .target(
             name: "main",
-            dependencies: []),
+            dependencies: ["class"]),
         .testTarget(
             name: "mainTests",
             dependencies: ["main"]),
